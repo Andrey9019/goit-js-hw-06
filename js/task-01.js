fb364f43875667cc88c431b5c09a211cee316618
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-07-26 00:53:38 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+const item = document.querySelectorAll(`h2`);
+console.log(`Number of categories:`, item.length);
+
+// const itemName = document.querySelector(`h2`);
+// console.log(`Category:`, itemName.textContent);
+// console.log();
+
+// const itemEl = document.querySelectorAll(
+//   item.forEach(function (number, index) {
+//     console.log(`Індекс ${index}, значення ${number}`);
+//   })
+// );
+// console.log(itemEl);
+
+// console.log(`Category: itemEl`, itemEl);
+// console.log(`Elements: `);
