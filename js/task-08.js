d30a588d0875c78fdd14fdc7530d7a2f14c268c4
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-08-01 23:54:50 +0300

    add hw 8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(`.login-form`);
+form.addEventListener(`submit`, handlerSubmit);
 
+function handlerSubmit(evt) {
+  evt.preventDefault();
+
+  const formObj = {};
+  const formElem = [...form.elements];
+
+  formElem.forEach((elem) => {
+    if (elem.type !== `submit`) {
+      formObj[elem.name] = elem.value;
+    }
+  });
+
+  console.log(formObj);
+  //   console.log(email.value);
+  //   console.log(password.value);
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChange = document.querySelector(`.change-color`);
+// console.log(btnChange);
+const colorSpan = document.querySelector(`.color`);
+// console.log(colorSpan);
+btnChange.addEventListener(`click`, handlerColor);
+function handlerColor(evt) {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
